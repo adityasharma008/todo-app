47c67a77e58daf3548030352b117a0a43b2c46b3
Aditya Sharma <121496528+AdityaSharma008@users.noreply.github.com>
2025-06-30 19:49:05 +0530

    setup basic routes and controllers

--- backend/app.js
+++ backend/app.js
@@ -1,1 +1,10 @@
-console.log("test");
+import express from 'express'
+import tasks from './routes/task.js'
+
+const app = express()
+
+app.use(express.json())
+app.use('/api/v1/tasks', tasks)
+
+const port = 5000
+app.listen(port, console.log(`Listening on port: ${port}`))
