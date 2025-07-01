9813921de32f7e76273a2b18a87963ca5700efb1
Aditya Sharma <121496528+AdityaSharma008@users.noreply.github.com>
2025-07-01 11:15:26 +0530

    add db connection

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,24 @@
 import express from 'express'
 import tasks from './routes/task.js'
+import { connectDB } from './db/connect.js'
+import dotenv from 'dotenv'
 
+dotenv.config()
 const app = express()
 
 app.use(express.json())
 app.use('/api/v1/tasks', tasks)
 
 const port = 5000
-app.listen(port, console.log(`Listening on port: ${port}`))
+
+const start = async() => {
+   try {
+      await connectDB(process.env.MONGO_URI)
+      app.listen(port, console.log(`Listening on port: ${port}`))
+   } catch(err) {
+      console.log(err)
+   }
+}
+
+start()
+
