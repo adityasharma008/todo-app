2bcfa45f52dd8e8d7fe9f3cfa04841db0d13230e
Aditya Sharma <121496528+AdityaSharma008@users.noreply.github.com>
2025-07-01 17:20:19 +0530

    crud operations

--- backend/controllers/task.js
+++ backend/controllers/task.js
@@ -1,20 +1,54 @@
 import Task from '../models/task.js'
 
-const getAllTasks = (req, res) => {
-   res.send('All Tasks')
+const getAllTasks = async (req, res) => {
+   try {
+      const tasks = await Task.find({})
+      res.status(200).json({ tasks })
+   } catch (err) {
+      res.status(500).json({ msg: err })
+   }
 }
 
 const createTask = async (req, res) => {
-   const task = await Task.create(req.body)
-   res.status(201).json({ task })
+   try {
+      const task = await Task.create(req.body)
+      res.status(201).json({ task })
+   } catch (err) {
+      res.status(400).json({ msg: err })
+   }
 }
 
-const updateTask = (req, res) => {
-   res.send('Update Task')
+const updateTask = async (req, res) => {
+   try {
+      const { id: taskID } = req.params
+      const task = await Task.findOneAndUpdate({ _id: taskID }, req.body, {
+                                                      new: true,
+                                                      runValidators: true
+                                                   })
+
+      if (!task) {
+         return res.status(404).json({ msg: `No task with id: ${taskID}` })
+      }
+
+      res.status(200).json({ task })
+   } catch (err) {
+      res.status(400).json({ msg: err })
+   }
 }
 
-const deleteTask = (req, res) => {
-   res.send('Delete Task')
+const deleteTask = async (req, res) => {
+   try {
+      const {id: taskID} = req.params 
+      const task = await Task.findOneAndDelete({_id:taskID})
+      
+      if(!task) {
+         return res.status(404).json({msg: `No task with id: ${taskID}`})
+      }
+
+      res.status(200).json({ task })
+   } catch (err) {
+      res.status(400).json({ msg: err })
+   }
 }
 
 export {getAllTasks, createTask, updateTask, deleteTask}
--- backend/models/task.js
+++ backend/models/task.js
@@ -1,7 +1,16 @@
 import mongoose from 'mongoose';
 
 const TaskSchema = new mongoose.Schema({
-   name:String, completed:Boolean
+   name:{
+      type: String,
+      required: [true, 'must provide name'],
+      trim: true,
+      maxlength: [25, 'name cannot be more than 25 chars']
+   },
+    completed:{
+      type: Boolean,
+      default: false
+    }
 })
 
 export default mongoose.model('Task', TaskSchema)
